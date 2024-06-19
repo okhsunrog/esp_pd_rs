d9381fb0ec22d92509a0c088d3f91ba0164b1c71
okhsunrog <me@okhsunrog.ru>
2024-06-19 17:46:36 +0300

    trying to bring up console, wip x1

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,66 @@ use esp_idf_svc::hal::{
     },
 };
 use heapless::Vec as HVec;
-use log::info;
+use log::{error, info};
 use smart_leds::{brightness, colors::*, SmartLedsWrite, RGB8};
 use std::{iter, thread, time::Duration};
 use ws2812_spi::Ws2812;
+use embedded_io_adapters::std::FromStd;
+use ufmt::{uwrite, uwriteln};
+
+
+use embedded_cli::cli::{CliBuilder, CliHandle};
+use embedded_cli::Command;
+use embedded_io;
+use std::convert::Infallible;
+use std::io;
+use std::io::{BufRead, Read};
+use std::ptr::null_mut;
+use embedded_io::Write;
+use esp_idf_svc::sys as _;
+use esp_idf_svc::sys::{esp, esp_vfs_dev_uart_use_driver, uart_driver_install};
+
+
+#[derive(Command)]
+enum Base<'a> {
+    /// Say hello to World or someone else
+    Hello {
+        /// To whom to say hello (World by default)
+        name: Option<&'a str>,
+    },
+
+    /// Stop CLI and exit
+    Exit,
+}
+
+pub struct Writer (FromStd<io::Stdout>);
+
+impl embedded_io::ErrorType for Writer {
+    type Error = Infallible;
+}
+
+impl embedded_io::Write for Writer {
+    fn write(&mut self, buf: &[u8]) -> Result<usize, Self::Error> {
+        self.0.write(buf).unwrap();
+        Ok(buf.len())
+    }
+
+    fn flush(&mut self) -> Result<(), Self::Error> {
+        self.0.flush().unwrap();
+        Ok(())
+    }
+}
+
 
 fn main() -> Result<()> {
     esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
+
+    unsafe {
+        esp!(uart_driver_install(0, 512, 512, 10, null_mut(), 0)).unwrap();
+        esp_vfs_dev_uart_use_driver(0);
+    }
+    
     let peripherals = Peripherals::take()?;
 
     let driver = SpiDriver::new_without_sclk(
@@ -30,9 +82,75 @@ fn main() -> Result<()> {
     info!("Spawning new thread for the WS2812 LED");
     let _handle = thread::spawn(move || blink_task(driver));
 
+
+    let mut embedded_writer: FromStd<io::Stdout> = FromStd::new(io::stdout());
+    embedded_writer.write_all(b"Writing using embedded_io\n").unwrap();
+    let _ = embedded_writer.flush();
+    println!("Writing using std");
+
+    let (command_buffer, history_buffer) = unsafe {
+        static mut COMMAND_BUFFER: [u8; 32] = [0; 32];
+        static mut HISTORY_BUFFER: [u8; 32] = [0; 32];
+        (COMMAND_BUFFER.as_mut(), HISTORY_BUFFER.as_mut())
+    };
+    let mut cli = CliBuilder::default()
+        .writer(embedded_writer)
+        .command_buffer(command_buffer)
+        .history_buffer(history_buffer)
+        .build()
+        .unwrap();
+
+
+    cli.write(|writer| {
+        uwrite!(
+            writer,
+            "Cli is running. Press 'Esc' to exit
+Type \"help\" for a list of commands.
+Use backspace and tab to remove chars and autocomplete.
+Use up and down for history navigation.
+Use left and right to move inside input."
+        )?;
+        Ok(())
+    })
+        .unwrap();
+
+    let mut buf = [0u8; 1];
+    let buf_ptr = buf.as_mut_ptr() as *mut libc::c_void;
+    
     loop {
-        thread::sleep(Duration::from_secs(60));
+        thread::sleep(Duration::from_millis(10));
+        let len = unsafe { libc::read(libc::STDIN_FILENO, buf_ptr, buf.len()) };
+        if len == 0 {
+            continue
+        }
+        
+
+        let byte = buf[0];
+
+        // Process incoming byte
+        // Command type is specified for autocompletion and help
+        // Processor accepts closure where we can process parsed command
+        // we can use different command and processor with each call
+        let _ = cli.process_byte::<Base, _>(
+            byte,
+            &mut Base::processor(|cli, command| {
+                match command {
+                    Base::Hello { name } => {
+                        // last write in command callback may or may not
+                        // end with newline. so both uwrite!() and uwriteln!()
+                        // will give identical results
+                        uwrite!(cli.writer(), "Hello, {}", name.unwrap_or("World"))?;
+                    }
+                    Base::Exit => {
+                        // We can write via normal function if formatting not needed
+                        cli.writer().write_str("Cli can't shutdown now")?;
+                    }
+                }
+                Ok(())
+            }),
+        );
     }
+
 }
 
 fn blink_task(driver: SpiDriver) -> Result<()> {
